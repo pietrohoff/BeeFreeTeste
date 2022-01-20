45714d48b56639eb827f4ab30f5065b18698844a
pmoctool <pietrohoff@gmail.com>
2022-01-20 15:54:28 -0300

    Update webpack.config.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,7 +5,7 @@ process.env.NODE_ENV = 'development'
 dotenv.config({ path: './.env' })
 
 
-const DEFAULT_PORT = process.env.PORT||'8080';
+const DEFAULT_PORT = process.env.PORT||'8081';
 console.log('listening on port: ', DEFAULT_PORT)
 
 export default {
